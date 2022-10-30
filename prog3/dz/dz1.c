c235c37168a23d97a879529ae4e1af600d117520
Andrey Gorev <andreygorev09@gmail.com>
2022-10-30 21:23:24 +0300

    ВЕТВЛЕНИЕ IF ELSE И ОПРЕДЕЛЕНИЕ БЛОКОВ ПРОГРАММЫ {}

---
+++ prog3/dz/dz1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t;
+    t = 35;
+    if (t > 10) 
+    {
+        printf("www\n");
+        printf("numeric t = %d\n", t);
+        if (t > 30)
+        {
+            printf("sss\n");
+            printf("numerik t = %d\n", t);
+        }
+        
+    }
+    else 
+    {
+        printf("ggg\n");
+        return 7;
+    }
+    return 0 ;
+}
