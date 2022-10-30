1c8fe9b2a78bfc631f788897ae884c92bcafb0dd
Andrey Gorev <andreygorev09@gmail.com>
2022-10-30 14:04:07 +0300

    ОПРЕДЕЛЕНИЕ ТИПА INT И FLOAT, ВЫВОД ЧЕРЕЗ СПЕЦИФИКАТОРЫ ДЛЯ КАЖДОГО ИЗ ТИПОВ

--- prog2/prog2.c
+++ prog2/prog2.c
@@ -3,9 +3,15 @@
 
 int main()
 {
-    int t;
-    t = 15;
-    t = 5 - t * 2 + 5; 
-    printf("%d\n", t);
+    int a;
+    float b;
+    float c;
+    a = 15;
+    a = 5 - a * 2 + 5; 
+    b = 3.14;
+    c = 2.54;
+    printf("variable a = %d\n", a);
+    printf("variable b = %f\n", b);
+    printf("variable c = %g\n", c);
     return 0;
 }
