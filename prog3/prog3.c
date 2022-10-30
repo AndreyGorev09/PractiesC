72df94889bfb01b64ef930418b0db56d9e95534c
Andrey Gorev <andreygorev09@gmail.com>
2022-10-30 14:31:39 +0300

    БЛОКИ ПРОЦЕДУР ВНУТРИ IF ELSE

--- prog3/prog3.c
+++ prog3/prog3.c
@@ -4,8 +4,12 @@
 int main()
 {
 	int t;
-	t = 5;
-	if(t > 10) printf("www\n");
+	t = 15;
+	if(t > 10) 
+	{
+		printf("www\n");
+		printf("numeric > 10\n");
+	}
 	else printf("ggg\n");
 	return 2;
 }
