d4dcc061fdfa289345a54f03bb05cf3df42cbc08
Andrey Gorev <andreygorev09@gmail.com>
2022-11-01 21:14:32 +0300

    КОММЕНТАРИИ, ПРЕНОС СТРОКИ, РАЗБОР ДЗ-3

---
+++ prog4/prog4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// ОДНОСТОЧНЫЙ КОММЕНТАРИЙ
+/* МНОНГОСТРОЧНЫЙ КОММЕНТАРИЙ*/
+
+
+int main(int argc, char const *argv[])
+{
+    int t;
+    t = 35;
+    if (t > 10)
+    {
+        printf("www\n");
+        if (t > 20)
+            printf("123\n");
+        if (t > 30)
+            printf("t = %d\n", t);
+    return 0;
+    }
+    else 
+    {
+        printf("ggg\n");
+        return 7;
+    }
+}
